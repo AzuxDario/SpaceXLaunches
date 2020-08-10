1a724b1b1ec74e5e7d8fea57bd77e9b464133ccd
AzuxDario <pawel_mibor95@tlen.pl>
2020-08-10 12:28:32 +0200

    Ramove unused imports

--- app/src/main/java/com/azuxdario/spacexlaunches/NextLaunchActivity.java
+++ app/src/main/java/com/azuxdario/spacexlaunches/NextLaunchActivity.java
@@ -6,8 +6,6 @@ import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import org.json.JSONException;
